30e5b4d9952c002ac08a86268b8929fabb090c16
Togene <jhonnywhiplash@hotmail.com.au>
2017-06-05 05:48:43 +1000

    Passangers Added, Clouds Shadows Kinda Done

---
+++ LonelyTrainRide/Assets/Head_Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Head_Movement : MonoBehaviour {
+
+    public Camera cam;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.rotation = cam.transform.rotation;
+	}
+}
---
+++ LonelyTrainRide/Assets/Reflection_Probe_Update.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reflection_Probe_Update : MonoBehaviour {
+
+    ReflectionProbe probe;
+    public int probeIterations;
+
+    void Awake()
+    {
+        probe = GetComponent<ReflectionProbe>();
+    }
+
+    void Update()
+    {
+        probe.transform.position = new Vector3(
+            Camera.main.transform.position.x,
+            Camera.main.transform.position.y,
+            Camera.main.transform.position.z
+        );
+
+        for(int i = 0; i < probeIterations; i++)
+        probe.RenderProbe();
+    }
+}
